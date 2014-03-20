a0ad994e7adf7f8fca8fbeab1fdd16f5debd2557
nnatsu <natsukani@gmail.com>
2014-03-20 01:17:43 -0400

    main and parse

--- main.c
+++ main.c
@@ -1,1 +1,36 @@
-ENFOIWENFOIHWEOIHOFIEHOIWENFKL
+#INCLUDE "parse.h"
+#INCLUDE "exec.h"
+
+typedef struct cground {
+  char* command; //single command, doen't includ ";&\n\t". To be parsed
+  char** argv;   //parsed parameters to be exec'd
+  int foreground;//1 (true) or 0 (false), to be used by exec
+} cground;
+
+
+int main (int argc, char argv**) {
+  int quit = 0;         //for main loop
+  char* input;          //input
+  struct cground info;  //to be passed in execute
+
+  //SIGACTION HERE
+
+  init();
+  while(!quit) {
+    printf("u3u ");
+
+    //parse input if it's not empty
+    if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
+      printf("bad input\n");
+    }
+    else {
+      while (input != WHITESPACE) {
+	info = chop(input); //chops info off of input
+	info.argv = parse(info.command); //parse into argv
+	execute(info);
+      }
+    }
+
+  }
+
+}

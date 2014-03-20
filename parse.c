8e0f4f636fec94ff950b03969bbbe4bd43bc5504
nnatsu <natsukani@gmail.com>
2014-03-20 03:01:16 -0400

    chop should be done

--- parse.c
+++ parse.c
@@ -1,9 +1,25 @@
-
+#include <string.h>
 
 char* chop (char*);
 char** parse (char*);
 
 char* chop(char* &input) {
+  int length, size;
+  char *block;
+  size_t i;
+  int quit;
+
+  quit = 0;
+
+  for (i = 0; i < strlen(input)|| !quit; i++) {
+    if (input[i] == '\n' || input[i] == '\t' || input[i] == '&' || input[i] == ';') {
+      strncpy(block, input, i+1);
+      strncpy(input, input+i, strlen(input)-i);
+      quit = 1;
+    }
+ 
+  }
+  return block;
   
 
 
@@ -19,7 +35,7 @@ char** parse (char* command) {
   int i, j; //i = command #, j = token # for current command
 
   token = strtok(command, "");
-  whilte(token) {
+  while(token) {
     get_rid_of_white_space(token);
     if (token ==";" || end_of_token(token) == ";") {
       j++; //move to next command

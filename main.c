37155c434f851cbcea66957afcf0c63e4de7836d
nnatsu <natsukani@gmail.com>
2014-03-20 02:10:55 -0400

    whaaaaa

--- main.c
+++ main.c
@@ -1,23 +1,21 @@
 #include "parse.h"
 #include "exec.h"
 
-#DEFINE 
+#define WHITESPACE " \n\t\r"
 
-typedef struct cground {
+typedef struct {
   char* command; //single command, doen't includ ";&\n\t". To be parsed
   char** argv;   //parsed parameters to be exec'd
   int foreground;//1 (true) or 0 (false), to be used by exec
-} cground;
+} Cground;
 
 
 int main (int argc, char argv**) {
   int quit = 0;         //for main loop
   char* input;          //input
-  struct cground info;  //to be passed in execute
+  Cground info;  //to be passed in execute
 
-  //SIGACTION HERE
   struct sigaction action;
- 
   action.sa_sigaction = &signal_handler;
 
   while(!quit) {
@@ -27,8 +25,9 @@ int main (int argc, char argv**) {
     if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
       printf("bad input\n");
     }
+
     else {
-      while (input != WHITESPACE) {
+      while (strspn(input, WHITESPACE) != strlen(input)) {
 	info = chop(input); //chops info off of input
 	info.argv = parse(info.command); //parse into argv
 	execute(info);

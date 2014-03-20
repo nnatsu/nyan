064b75e0e000e307b40e94431e027cfdc1a91844
nnatsu <natsukani@gmail.com>
2014-03-20 01:03:03 -0400

    job

--- job.c
+++ job.c
@@ -14,7 +14,7 @@ typedef struct {
     int status;
     Job *prev;
     Job *next;
-    char** args;
+    char **args;
 }Job;
 
 Job *head, tail;
@@ -24,17 +24,33 @@ void init_job() {
     tail = malloc(sizeof(Job));
     head->next = tail;
     tail->prev = head;
+    tail->jid = 0;
 }
 
-void add_job(pid_t char**) {
+void add_job(pid_t pid, int status, char ** args) {
+    Job *new = malloc(sizeof(Job));
+    new->pid = pid;
+    new->jib = ++(tail->jid);
+    new->status = status;
+    new->args = args;
+    new->prev = tail->prev;
+    new->next = prev;
+    prev->prev = new;
+}
+
+void delete_job(pid_t pid) {
+    Job *cur = head;
+    do {
+        cur = cur->next;
+    } while (cur->pid != pid && cur != NULL);
+    cur->prev->next = cur->next;
+    cur->next->prev = cur->prev;
+    free(cur);
+}
+
+Job* find_job(int) {
     
 }
-
-void delete_job(pid_t);
-
-
-
-Job* find_job(int);
 
 void print_job();
 

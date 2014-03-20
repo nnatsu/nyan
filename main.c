53e3889a5f6c02b019b7633aa526f72f92d63899
nnatsu <natsukani@gmail.com>
2014-03-20 06:27:43 -0400

    h

--- main.c
+++ main.c
@@ -10,57 +10,57 @@
 #define WHITESPACE " \n\t\r"
 #define BUFFER_SIZE 256
 
+sigset_t blockmask;
+sem_t mutex;
+struct termios shell_tmodes;
+int shell_terminal;
+pid_t sid; //for main shell
 
 int main (int argc, char **argv) {
-  int quit = 0; //for main loop
-  int length = 0;
-  char *input;   //input
-  Cground info;                   //to be passed in execute
+    int quit = 0; //for main loop
+    int length = 0;
+    char *input;   //input
+    Cground info;                   //to be passed in execute
 
-  struct sigaction action;
-  action.sa_handler = &signal_handler;
-  if (sigaction(SIGCHLD, &action, NULL) < 0) {
-    perror("sigaction");
-    return 1;
-  }
+    struct sigaction action;
+    action.sa_handler = &signal_handler;
+    if (sigaction(SIGCHLD, &action, NULL) < 0) {
+        perror("sigaction");
+        return 1;
+    }
 
-    
-    sigset_t blockmask;           //Initialize set for blocking SIGCHLD
-    sigemptyset(&blockmask);
+    sigemptyset(&blockmask);        //Initialize set for blocking SIGCHLD
     sigaddset(&blockmask, SIGCHLD);
+
+    sem_init(&mutex, 1, 1); ////Initialize MUTEX
     
-    //semaphore in MAIN????????     //Initialize MUTEX
-    sem_t mutex;
-    sem_init(&mutex, 1, 1); //shared semaphore
-    
-    struct termios shell_tmodes;
-    int shell_terminal = STDIN_FILENO;       //fd and pid for main shell
-    pid_t sid = getpid();
+
+    shell_terminal = STDIN_FILENO;       //fd and pid for main shell
+
+    sid = getpid();                     //get main shell pid
     setpgid(sid, sid);
     tcsetpgrp(shell_terminal, sid);
     tcgetattr(shell_terminal, &shell_tmodes);
     
 
-  while(!quit) {
-    printf("u3u ");
+    while(!quit) {
+        printf("u3u ");
 
-    //parse input if it's not empty
-    if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
-      printf("bad input\n");
+        //parse input if it's not empty
+        if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
+            printf("bad input\n");
+        } else {
+            while (strspn(input, WHITESPACE) != strlen(input)) {
+                info.command = chop(input); //chops info off of input
+                length = strlen(info.command); //nyaaaan
+                input = input + length;
+                info.argv = parse(info.command); //parse into argv
+                execute(info);
+            }
+        }
+
     }
 
-    else {
-      while (strspn(input, WHITESPACE) != strlen(input)) {
-	info.command = chop(input); //chops info off of input
-	length = strlen(info.command); //nyaaaan
-	input = input + length;
-	info.argv = parse(info.command); //parse into argv
-	execute(info);
-      }
-    }
-
-  }
-
-  return 0;
+    return 0;
 
 }
--- makefile
+++ makefile
@@ -1,22 +1,8 @@
 CC = gcc
-CFLAGS = -g -Wall
+CFLAGS = -c -Wall
 
-All:	mysh
-
-mysh:	main.o parse.o job.o exec.o
-	$(CC) main.o parse.o job.o exec.o -o mysh
-
-main.o: main.c
-	$(CC) $(CFLAGS) -c main.c
-
-parse.o:parse.c parse.h
-	$(CC) $(CFLAGS) -c parse.c
-
-job.o: job.c
-	$(CC) $(CFLAGS) -c job.c
-
-exec.o: exec.c
-	$(CC) $(CFLAGS) -c exec.c
+main: main.o parse.o job.o exec.o
+	$(CC) -o main main.o parse.o job.o exec.o -lpthread
 
 clean:
-	rm *.o mysh
+	$(RM) main *.o
